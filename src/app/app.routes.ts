c7afca25dbfda1311a93652ec3dec4b7a7b0b490
Jobson Oliveira <jobsondeveloper@gmail.com>
2024-06-21 16:51:33 -0300

    Tow code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
-    pathMatch: 'full',
-  },
-  {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
   {
     path: 'home',
